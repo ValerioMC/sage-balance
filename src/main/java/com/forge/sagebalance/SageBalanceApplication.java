5746c032a3800999adf7dac95debb0610a94ab68
Valerio Mario Casale <valerio.casale@gmail.com>
2023-12-23 20:00:44 +0100

    ionitial commit

---
+++ src/main/java/com/forge/sagebalance/SageBalanceApplication.java
@@ -0,0 +1,13 @@
+package com.forge.sagebalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SageBalanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SageBalanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/forge/sagebalance/SageBalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forge.sagebalance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SageBalanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
